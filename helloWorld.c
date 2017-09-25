d7b6c608db96adecc8bbd54e5293cdca725e646e
joe5893 <3137359368@qq.com>
2017-09-25 12:22:41 +0800

    add helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,15 @@
+/*
+Author: ¹ù°²½à£¬Ñ§ºÅ£º2017141461297
+Time: 2017/9/24 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("Hello World!");
+	
+	return 0;
+}
